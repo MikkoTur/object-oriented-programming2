dbfb5d1f097c9d51285c818a01c2133381c5cc37
MikkoTur <mikko.turppo@hotmail.com>
2018-01-11 11:04:39 +0200

    TaskPerson example done

---
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Person personMikko = new Person();
+            personMikko.Name = "Mikko T";
+            personMikko.GrowOld();
+            personMikko.PrintPersonInfo();
+
+            Person newPerson = new Person("Lil Pump", 18);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+
+
+        }
+    }
+}
