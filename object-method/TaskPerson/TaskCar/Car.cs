00f52e04fadedce5c6c5c73c7d29b8b87fbd8fc8
MikkoTur <mikko.turppo@hotmail.com>
2018-01-12 11:03:39 +0200

    taskCar 1.0

---
+++ object-method/TaskPerson/TaskCar/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        //Car info
+        public string Brand;
+        public double Speed;
+
+        //Constructor
+
+        public Car()
+        {
+            Brand = null;
+            Speed = 0;
+        }
+
+        //Methods
+
+        public void AskData()
+        {
+            Console.WriteLine($"Syötä auton merkki ja nopeus km/h: ");
+            Brand = Console.ReadLine();
+            Speed = int.Parse(Console.ReadLine());
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on: {Brand}\nAuton nopeus on: {Speed}\n");
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine($"Anna kerroin jolla auton nopeuttasi lisätään: ");
+            double speed1 = double.Parse(Console.ReadLine());
+            if (speed1 < 1)
+                Console.WriteLine($"Negatiivistä lukua ei sallita");
+            else
+                Console.WriteLine($"Nopeutta lisättii {speed1} kertaiseksi, uusi nopeus on {speed1 * Speed}km/h\n");
+
+        }
+
+    }
+}
---
+++ object-method/TaskPerson/TaskCar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Car newCar = new Car();
+            newCar.AskData();
+            newCar.ShowCarInfo();
+            newCar.Accelerate();
+
+            Car newCar2 = new Car();
+            newCar2.AskData();
+            newCar2.ShowCarInfo();
+            newCar2.Accelerate();
+
+            Console.ReadKey();
+
+        }
+    }
+}
