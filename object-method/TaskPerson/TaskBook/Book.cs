ff7e5d112a970b83bc17b1caa03500a605487afa
MikkoTur <mikko.turppo@hotmail.com>
2018-01-12 09:39:49 +0200

    TaskBook done

---
+++ object-method/TaskPerson/TaskBook/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBook
+{
+    class Book
+    {
+        //Kirjan tiedot
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title}\n" +
+                $"Kirjailija: {Author}\n" +
+                $"Id-Numero: {Id}\n" +
+                $"Hinta: {Price}\n");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return ($"{this.Title} on kalliimpi kuin {book.Title} kirja\n");
+            else
+                return ($"{book.Title} on kalliimpi kuin {this.Title} kirja\n");
+
+        }
+    }
+}
---
+++ object-method/TaskPerson/TaskBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Book newBook = new Book("Dabbers", "Seppo Aho", 1234567, 29.99);
+            newBook.PrintBookInfo();
+
+            Book newBook2 = new Book("Lit", "Lil Pump", 7654321, 45.99);
+            newBook2.PrintBookInfo();
+
+            Book newBook3 = new Book("How to be Atte", "Atte Peltonen", 7657231, 14.99);
+            newBook3.PrintBookInfo();
+
+            Console.WriteLine($"{newBook2.CompareBook(newBook)}");
+            Console.WriteLine($"{newBook3.CompareBook(newBook)}");
+
+            Console.ReadKey();
+        }
+    }
+}
