2198ebdd43e4190c03c2dd992372ffa6d07ba77e
MikkoTur <mikko.turppo@hotmail.com>
2018-01-18 10:59:40 +0200

    TaskFirma done

---
+++ object-method/TaskPerson/TaskFirma/Firma.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFirma
+{
+    class Firma
+    {
+        public string Title;
+        public string Address;
+        public string Phone;
+        public double Outcome;
+        public double Expense;
+
+        //Constructor
+        public Firma(string title, string address, string phone, double outcome, double expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Outcome = outcome;
+            Expense = expense;
+        }
+
+        //Constructor määritelmä
+        public Firma()
+        {
+            Title = "Ei tiedossa";
+            Address = "Ei tiedossa";
+            Phone = "Ei tiedossa";
+            Outcome = 0;
+            Expense = 0;
+        }
+
+        //Kopiomuodostin
+        public Firma(Firma company)
+        {
+            Title = company.Title;
+            Address = company.Address;
+            Phone = company.Phone;
+            Outcome = company.Outcome;
+            Expense = company.Expense;
+        }
+
+        //Method
+        public void AskData()
+        {
+            Console.WriteLine($"Syötä yrityksesi tiedot: Nimi, Osoite, Puhelinnumero, Tulot ja Menot");
+            Title = Console.ReadLine();
+            Address = Console.ReadLine();
+            Phone = Console.ReadLine();
+            Outcome = double.Parse(Console.ReadLine());
+            Expense = double.Parse(Console.ReadLine());
+        }
+
+        public void ShowCompanyInfo()
+        {
+            Console.WriteLine($"Yrityksen nimi on: {Title}\n" +
+                $"Osoite: {Address}\n" +
+                $"Puhelinnumero: {Phone}\n" +
+                $"Tulot: {Outcome}\n" +
+                $"Menot: {Expense}");
+
+        }
+        //Method firman voittoprosentti
+
+        public void CompanyProfitCalculate()
+        {
+            double profit = (Outcome - Expense) / Expense * 100;
+
+            Console.WriteLine($"Yrityksesi voittoprosentti on: {profit:f2}");
+
+            if (profit < 100)
+            {
+                Console.WriteLine($"Yrityksesi tulos on kehno");
+            }
+            if (profit <= 300)
+            {
+                Console.WriteLine($"Yrityksesi tulos on tyydyttävä");
+            }
+            if (profit > 300)
+            {
+                Console.WriteLine($"Yrityksesi tulos on kiitettävä");
+            }
+        }
+    }
+}
---
+++ object-method/TaskPerson/TaskFirma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskFirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Firma newfirma = new Firma();
+            newfirma.AskData();
+            newfirma.ShowCompanyInfo();
+            newfirma.CompanyProfitCalculate();
+
+            Console.ReadLine();
+        }
+    }
+}
