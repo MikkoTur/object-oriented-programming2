4bc275f94d36365461256fd84b9416547ac40320
MikkoTur <mikko.turppo@hotmail.com>
2018-01-19 09:16:58 +0200

    TaskEmployee Done

---
+++ object-method/TaskPerson/TaskEmployee/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public double Salary;
+
+        //Constructor
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijän nimi: {Name}\n" +
+                $"Id-numero: {Id}\n" +
+                $"Asema: {Position}\n" +
+                $"Palkka: {Salary}\n");
+        }
+
+        //Methods
+        public void CompareSalary(Employee employee)
+        {
+            if (Salary < employee.Salary)
+            {
+                Console.WriteLine($"\n{employee.Name} palkka on isompi kuin henkilön {Name} palkka");
+            }
+            else
+            {
+                Console.WriteLine($"\n{Name} palkka on isompi kuin henkilön {employee.Name} palkka");
+            }
+        }
+    }
+}
---
+++ object-method/TaskPerson/TaskEmployee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Seppo Taalasmaa", 1234, "Toimitusjohtaja", 5500);
+            employees[1] = new Employee("Ismo Laitela", 6666, "Sihteeri", 2200);
+            employees[2] = new Employee("Jörgen Balls", 4321, "Omistaja", 15000);
+
+            for (int i = 0; i < 3; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+            employees[1].CompareSalary(employees[2]);
+            employees[0].CompareSalary(employees[1]);
+            employees[2].CompareSalary(employees[1]);
+
+            Console.ReadKey();
+        }
+    }
+}
