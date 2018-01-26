768bc32adc92998b5112bd91989a35df38dd4bf4
MikkoTur <mikko.turppo@hotmail.com>
2018-01-26 09:52:11 +0200

    Kirja ja kirjailija kesken

---
+++ object-method/Book_Writer/Book_Writer/Book.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Writer
+{
+    class Book
+    {
+        public string Name;
+        public string Writer;
+        public string Publisher;
+        public double Price;
+        public string Theme;
+
+        static string ChangeTheme;
+
+        //Constructor
+        public Book()
+        {
+            Name = "Unknown";
+            Writer = "Unknown";
+            Publisher = "Unknown";
+            NewPrice = 0;
+            Theme = "Unknown";
+        }
+
+
+
+        public Book(string Name, string Writer, string Publisher, double Price, string Theme)
+        {
+            this.Name = Name;
+            this.Writer = Writer;
+            this.Publisher = Publisher;
+            this.Price = Price;
+            this.Theme = Theme;
+
+        }
+        public Book(Book book)
+        {
+            this.Name = book.Name;
+            this.Writer = book.Writer;
+            this.Publisher = book.Publisher;
+            this.Price = book.Price;
+            this.Theme = book.Theme;
+
+        }
+
+        //Methods
+
+        public void SearchBook(Book book)
+        {
+            if (this.Name.Equals(book.Name))
+                Console.WriteLine($"{book.Name}\n" +
+                    $"{book.Writer}\n" +
+                    $"{book.Publisher}\n" +
+                    $"{book.Price}\n" +
+                    $"{book.Theme}\n");
+            else
+                Console.WriteLine($"Kirjaa: {book.Name}, ei löytynyt");
+        }
+
+        public void SearchBook(string name)
+        {
+            if (Name.Equals(name))
+                Console.WriteLine($"{Name}\n" +
+                    $"{Writer}\n" +
+                    $"{Publisher}\n" +
+                    $"{Price}\n" +
+                    $"{Theme}\n");
+            else
+                Console.WriteLine($"Kirjaa: {Name}, ei löytynyt");
+        }
+
+
+        public double NewPrice
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                if (value > 30)
+                {
+                    Price = value * 0.9;
+                    Console.WriteLine($"Kirjan {Name} uusi hinta on {Price}");
+                }
+            }
+        }
+
+
+
+        public static void ThemeChange()
+        {
+            Console.WriteLine(ChangeTheme);
+        }
+    }
+}
---
+++ object-method/Book_Writer/Book_Writer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Book_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Book uusiKirja1 = new Book("Nasse sedän muistot", "Jörgen Balls", "OTAVA", 35.99, "Elämänkerta");
+            uusiKirja1.SearchBook(uusiKirja1);
+            uusiKirja1.NewPrice = 35.99;
+            Book.ThemeChange();
+
+            Console.WriteLine("-------------------------------------");
+
+
+            Book uusiKirja2 = new Book("Riitan Reseptit", "Seppo Jormakka", "Tokmanni", 9.99, "Ruokakirja");
+            uusiKirja2.SearchBook(uusiKirja1);
+            uusiKirja2.NewPrice = 9.99;
+            Book.ThemeChange();
+
+
+            Console.WriteLine("-------------------------------------");
+
+            Book uusiKirja3 = new Book("Reitti Mokadishuun", "Aku Hirviniemi", "WSOY", 50.50, "Matkailu");
+            uusiKirja1.SearchBook(uusiKirja3);
+            uusiKirja3.NewPrice = 50.50;
+            Book.ThemeChange();
+
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Kirja löytyi!");
+            string searchBook = "Nasse sedän muistot";
+            uusiKirja1.SearchBook(searchBook);
+
+
+            Console.ReadKey();
+        }
+    }
+}
