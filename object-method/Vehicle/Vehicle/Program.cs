d63fd53063ab1c3afe197d36529369d38411a2ce
MikkoTur <mikko.turppo@hotmail.com>
2018-02-01 10:20:10 +0200

    update

---
+++ object-method/Vehicle/Vehicle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Vehicle uusiVehicle1 = new Vehicle("Urheilu", "Ferrari", "2015", 300000);
+            uusiVehicle1.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ object-method/Vehicle/Vehicle/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle
+{
+    class Vehicle
+    {
+        public string Type;
+        public string Brand;
+        public string Year;
+        public double Price;
+
+        //Constructor
+
+        public Vehicle(string type, string brand, string year, double price)
+        {
+            Type = type;
+            Brand = brand;
+            Year = year;
+            Price = price;
+        }
+
+        public Vehicle()
+        {
+            Type = "Ei tiedoissa";
+            Brand = "Ei tiedoissa";
+            Year = "Ei tiedoissa";
+            Price = 0;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Auton tyyppi: {Type}\n" +
+                $"Auton merkki: {Brand}\n" +
+                $"Vuosimalli: {Year}\n" +
+                $"Hinta: {Price:C}");
+        }
+    }
+}
