3fb126f73b64d00b686fa2d3b6728f0efa9d4f60
MikkoTur <mikko.turppo@hotmail.com>
2018-02-16 10:20:04 +0200

    bban started

---
+++ object-method/Bban/Bban/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string machineFormat = "227720-35988";
+            bool isMachineFormat = bban.Correctnumber(ref machineFormat);
+            if (isMachineFormat)
+            {
+                Console.WriteLine($"{machineFormat} - {bban.IsValidAccount(machineFormat)}");
+            }
+            else
+            {
+                Console.WriteLine("Account number incorrect!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
