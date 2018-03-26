c474e0c459e2a03be1a7c72972106370310ba69c
MikkoTur <mikko.turppo@hotmail.com>
2018-03-26 10:31:26 +0300

    BankApp Done

--- object-method/BankApp/BankApp/Program.cs
+++ object-method/BankApp/BankApp/Program.cs
@@ -17,27 +17,41 @@ namespace BankApp
             customers.Add(new Customer(bank.CreateAccount(), "Aku", "Ankka"));
             customers.Add(new Customer(bank.CreateAccount(), "Hannu", "Hanhi"));
 
-            bank.AddTransactionForCustomer(customers[0].AccountNumber, 
-                new Transaction(1234, new DateTime(2018, 03, 23)));
-            bank.AddTransactionForCustomer(customers[1].AccountNumber,
-                new Transaction(6669, new DateTime(2018, 03, 21)));
-            bank.AddTransactionForCustomer(customers[2].AccountNumber,
-                new Transaction(2330, new DateTime(2018, 03, 23)));
-            bank.AddTransactionForCustomer(customers[0].AccountNumber,
-                new Transaction(550, new DateTime(2018, 03, 26)));
-            bank.AddTransactionForCustomer(customers[1].AccountNumber,
-                new Transaction(2000, new DateTime(2018, 03, 18)));
-            bank.AddTransactionForCustomer(customers[2].AccountNumber,
-                new Transaction(1600, new DateTime(2018, 03, 25)));
+            Random rnd = new Random();
+            for (int i = 0; i < 150; i++)
+            {
+                bank.AddTransactionForCustomer(customers[rnd.Next(0, 3)].AccountNumber,
+                    new Transaction(rnd.Next(-3000, 5000),
+                    new DateTime(2017, rnd.Next(1, 13), rnd.Next(1, 28))));
+            }
 
-            PrintBalance(bank, customers[0]);
-            PrintBalance(bank, customers[1]);
-            PrintBalance(bank, customers[2]);
+            //bank.AddTransactionForCustomer(customers[0].AccountNumber, 
+            //new Transaction(1234, new DateTime(2018, 03, 23)));
+            //bank.AddTransactionForCustomer(customers[1].AccountNumber,
+            //new Transaction(6669, new DateTime(2018, 03, 21)));
+            //bank.AddTransactionForCustomer(customers[2].AccountNumber,
+            //new Transaction(2330, new DateTime(2018, 03, 23)));
+            //bank.AddTransactionForCustomer(customers[0].AccountNumber,
+            //new Transaction(550, new DateTime(2018, 03, 26)));
+            //bank.AddTransactionForCustomer(customers[1].AccountNumber,
+            //new Transaction(2000, new DateTime(2018, 03, 18)));
+            //bank.AddTransactionForCustomer(customers[2].AccountNumber,
+            //new Transaction(1600, new DateTime(2018, 03, 25)));
 
-            var endTime = DateTime.Today;
+            for (int i = 0; i < customers.Count; i++)
+            {
+                PrintBalance(bank, customers[i]);
+            }
+
+            //PrintBalance(bank, customers[0]);
+            //PrintBalance(bank, customers[1]);
+            //PrintBalance(bank, customers[2]);
+
+            //var endTime = DateTime.Today;
+            var endTime = new DateTime(2017, 12, 31);
             var time = new TimeSpan(24 * 30 * 6, 0, 0);
-            var startTime = endTime.Date - time;
-            Console.WriteLine($"Tilitapahtumat viimeisen kuuden kuukauden ajalta: {startTime.ToShortDateString()}" +
+            var startTime = new DateTime(2017, 01, 01);//endTime.Date - time;
+            Console.WriteLine($"Tilipahatumat viimeisen vuoden ajalta: {startTime.ToShortDateString()}" +
                 $"-{endTime.ToShortDateString()}");
 
             PrintTransactions(bank.GetTransactionsForCustomerForTimeSpan(customers[0].AccountNumber, 
@@ -46,6 +60,7 @@ namespace BankApp
                 startTime, endTime), customers[1]);
             PrintTransactions(bank.GetTransactionsForCustomerForTimeSpan(customers[2].AccountNumber,
                 startTime, endTime), customers[2]);
+
             Console.WriteLine("<Enter> lopettaa!");
             Console.ReadKey();
         }
