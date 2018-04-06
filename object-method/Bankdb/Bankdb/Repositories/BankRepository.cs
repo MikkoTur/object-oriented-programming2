9d2f41d7594bcfd905a040ebb3d4c3ee89e93e51
MikkoTur <mikko.turppo@hotmail.com>
2018-04-06 08:51:45 +0300

    bankdb update

---
+++ object-method/Bankdb/Bankdb/Repositories/BankRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bankdb.Models;
+
+namespace Bankdb.Repositories
+{
+    class BankRepository : IBank
+    {
+        public void Create(Bank bank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Bank bank)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
